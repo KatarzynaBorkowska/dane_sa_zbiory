02bdd27999eaa30d1b78fee6d6241dda6fced9c7
Kasia <71874062+KatarzynaBorkowska@users.noreply.github.com>
2021-05-08 22:36:03 +0200

    Update main.cpp

--- dane_sa_zbiory/main.cpp
+++ dane_sa_zbiory/main.cpp
@@ -1,14 +1,100 @@
-//
-//  main.cpp
-//  dane_sa_zbiory
-//
-//  Created by Kasia on 12/04/2021.
-//
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int main() {
+    int a1, a2;
+    int b1, b2;
+    cout << "Zbior A: ";
+    cin >> a1 >> a2;
+    cout << "Zbiór B: ";
+    cin >> b1 >> b2;
+    int i,n = 0,x,y;
+    cout << "Podaj x i y: ";
+    cin >> x >> y;
+    for(i = a1;i <= a2;i++) n++;
+    cout << "Moc zbioru A " << n << endl;
+    n = 0;
+    for(i = b1;i <= b2;i++) n++;
+    cout << "Moc zbioru B " << n << endl;
+    
+    int m = 0;
+    n = 0;
+    for(i = 1;i <= a2;i++)
+        if(i % x == 0)
+            m++;
+    for(i = 1;i < a1;i++)
+        if(i % x == 0)
+            n++;
+    cout << "Liczba liczb w zbiorze A podzielnych przez " << x << ": " << m - n << endl;
+    
+    m = 0;
+    n = 0;
+    for(i = 1;i <= b2;i++)
+        if(i % x == 0)
+            m++;
+    for(i = 1;i < b1;i++)
+        if(i % x == 0)
+            n++;
+    cout << "Liczba liczb w zbiorze B podzielnych przez " << x << ": "  << m - n << endl;
+    
+    m = 0;
+    n = 0;
+    for(i = 1;i <= a2;i++)
+        if(i % y != 0)
+            m++;
+    for(i = 1;i < a1;i++)
+        if(i % y != 0)
+            n++;
+    cout << "Liczba liczb w zbiorze A niepodzielnych przez " << y << ": "  << m - n << endl;
+    
+    m = 0;
+    n = 0;
+    for(i = 1;i <= b2;i++)
+        if(i % y != 0)
+            m++;
+    for(i = 1;i < b1;i++)
+        if(i % y != 0)
+            n++;
+    cout << "Liczba liczb w zbiorze B niepodzielnych przez " << y << ": " << m - n << endl;
+    
+    m = 0;
+    n = 0;
+    for(i = 1;i <= a2;i++)
+        if(i % y == 0 && i % x == 0)
+            m++;
+    for(i = 1;i < a1;i++)
+        if(i % y == 0 && i % x == 0)
+            n++;
+    cout << "Liczba liczb w zbiorze A podzielnych jednocześnie przez " << x << " i " << y << ": " << m - n << endl;
+    
+    m = 0;
+    n = 0;
+    for(i = 1;i <= b2;i++)
+        if(i % y == 0 && i % x == 0)
+            m++;
+    for(i = 1;i < b1;i++)
+        if(i % y == 0 && i % x == 0)
+            n++;
+    cout << "Liczba liczb w zbiorze B podzielnych jednocześnie przez " << x << " i " << y << ": "  << m - n << endl;
+    
+    m = 0;
+    n = 0;
+    for(i = 1;i <= a2;i++)
+        if(i % y != 0 && i % x == 0)
+            m++;
+    for(i = 1;i < a1;i++)
+        if(i % y != 0 && i % x == 0)
+            n++;
+    cout << "Liczba liczb w zbiorze A podzielnych jednocześnie przez " << x << " i jednocześnie niepodzielnych przez " << y << ": " << m - n << endl;
+    
+    m = 0;
+    n = 0;
+    for(i = 1;i <= b2;i++)
+        if(i % y != 0 && i % x == 0)
+            m++;
+    for(i = 1;i < b1;i++)
+        if(i % y != 0 && i % x == 0)
+            n++;
+    cout << "Liczba liczb w zbiorze A podzielnych jednocześnie przez " << x << " i jednocześnie niepodzielnych przez " << y << ": " << m - n << endl;
     return 0;
 }
